b007c939c73938459511a33b54b9f070ccad7816
Mário Oliveira <oiramlopes@hotmail.com>
2022-10-29 02:27:25 -0300

    Finalização do post e put

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require ('express')
+const bodyParser = require('body-parser')
+
 const app = express()
 
 const port = 3000
+
+app.use(bodyParser.json())
 
 app.post('/hello', (req, res) => { //arrow function
     res.send('Hello World 1')
@@ -21,23 +25,40 @@ const mensagens = ["Essa é a primeira mensagem",
                   "Essa é a segunda mensagem"]
 
 //[GET] /mensagens - Retorna a lista de mensagem
-app.get('/mensagem', (req, res) => {
+app.get('/mensagens', (req, res) => {
     res.send(mensagens)
 })
 
 //[GET] /mensagem/{id} - Retorna apenas uma única mensagem pelo ID
-app.get('/mensagem/:id', (req, res) => {
+app.get('/mensagens/:id', (req, res) => {
     const id = req.params.id - 1
     const mensagem = mensagens [id] 
     res.send(mensagem)
 })
     
 //[POST] / mensagens - Cria uma nova mensagem
+app.post('/mensagens', (req, res) => {
+    const mensagem = req.body.mensagem//Recebe as mesasgens informadas no body do Postman
+    mensagens.push(mensagem)//Adiciona mensagens na lista
+    res.send(`Mensagem criada com sucesso: ${mensagem}.`)
+})
 
 
 //[PUT] /mensagens/{id} - Atualiza uma mensagem pelo ID
+app.put('/mensagens/:id', (req, res) => { //obter o id
+    const id = req.params.id - 1 //Qual mensagem vai ser editada
+    const mensagem = req.body.mensagem //pega a nova mensagem
+    mensagens[id] = mensagem //atualiza a nova  mensagem na posição do id
+    res.send(`Mensagem atualizada com sucesso: ${mensagem}.`)//mostra nova mensagem
+
+
+})
+
+
+
+
 //[DELETE] /mensagens/{id} - Remove uma mensagem pelo ID
 
 app.listen(port, () => {// => arrow function
-    console.log(`App rodando na porta: ${port} `)
+    console.log(`App rodando na porta: ${port} `)//template stream
 })
